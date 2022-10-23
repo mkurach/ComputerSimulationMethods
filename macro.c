02ed29eb4c972a91056ce0d071aa46039262ff98
mkurach <96025745+mkurach@users.noreply.github.com>
2022-10-23 19:08:13 +0200

    working simulation
    energy is growing slowly

---
+++ macro.c
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <fstream>
+#include <TFile.h>
+#include <TH2D.h>
+#include <TH1D.h>
+#include <TLine.h>
+#include <TCanvas.h>
+void macro() {
+    //FILLING FROM TXT FILE
+    ifstream file;
+    file.open("pedy.txt");
+
+    Float_t px,py,pz;
+    TH1D* histx = new TH1D("x","x",100,-7,7);
+    TH1D* histy = new TH1D("y","y",50,-50,50);
+    TH1D* histz = new TH1D("z","z",50,-50,50);
+
+    while(file>>px && file>>py && file>>pz){
+        histx->Fill(px);
+        histy->Fill(py);
+        histz->Fill(pz);
+    }
+
+    histx->Draw();
+
+}
